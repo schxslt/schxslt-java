c08f0f5d766257808f0433cb1913f559925f066e
David Maus <dmaus@dmaus.name>
2020-09-23 11:46:14 +0200

    Declare Resolver.resolver to be private

--- src/main/java/name/dmaus/schxslt/Resolver.java
+++ src/main/java/name/dmaus/schxslt/Resolver.java
@@ -36,7 +36,7 @@ import javax.xml.transform.stream.StreamSource;
 
 class Resolver implements URIResolver
 {
-    final org.xmlresolver.Resolver resolver = new org.xmlresolver.Resolver();
+    final private org.xmlresolver.Resolver resolver = new org.xmlresolver.Resolver();
 
     Resolver ()
     {}
